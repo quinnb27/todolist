cc13ae69a5025bc6422f3af8f7a05570aba448c2
quinnb27 <quinnb27@gmail.com>
2014-06-05 14:34:31 -0700

    Seperate Pages

--- todolist/src/com/example/todolist/MainActivity.java
+++ todolist/src/com/example/todolist/MainActivity.java
@@ -82,7 +82,18 @@ public class MainActivity extends ActionBarActivity {
 			// getItem is called to instantiate the fragment for the given page.
 			// Return a PlaceholderFragment (defined as a static inner class
 			// below).
-			return PlaceholderFragment.newInstance(position + 1);
+			
+			//The Page Selector 0 contains puppy, 1 contains todos
+			switch (position) {
+			
+			case 0:
+				return PuppyPage.newInstance();
+
+			case 1:
+				return ListPage.newInstance();
+			}
+			
+			return null;
 		}
 
 		@Override
@@ -104,39 +115,46 @@ public class MainActivity extends ActionBarActivity {
 		}
 	}
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		private static final String ARG_SECTION_NUMBER = "section_number";
+	public static class PuppyPage extends Fragment {
 
 		/**
-		 * Returns a new instance of this fragment for the given section number.
+		 * Returns a new instance of this fragment.
 		 */
-		public static PlaceholderFragment newInstance(int sectionNumber) {
-			PlaceholderFragment fragment = new PlaceholderFragment();
-			Bundle args = new Bundle();
-			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-			fragment.setArguments(args);
+		public static PuppyPage newInstance() {
+			PuppyPage fragment = new PuppyPage();
 			return fragment;
 		}
 
-		public PlaceholderFragment() {
+		public PuppyPage() {
 		}
 
 		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			TextView textView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			textView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			
+			View rootView = inflater.inflate(R.layout.puppy_page, container, false);
+			
+			return rootView;
+		}
+	}
+	
+	public static class ListPage extends Fragment {
+
+		/**
+		 * Returns a new instance of this fragment.
+		 */
+		public static ListPage newInstance() {
+			ListPage fragment = new ListPage();
+			return fragment;
+		}
+
+		public ListPage() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			
+			View rootView = inflater.inflate(R.layout.list_page, container, false);
+			
 			return rootView;
 		}
 	}
