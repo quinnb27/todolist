4c1cc642167f488e3b5cde18febd3e276501e944
clareification <Clare@10.8.103.108>
2014-06-09 09:47:37 -0700

    Added classes for puppy/list pages

---
+++ todolist/src/com/example/todolist/Puppy.java
@@ -0,0 +1,21 @@
+package com.example.todolist;
+
+public class Puppy {
+
+	//Declare variables- how happy the puppy is
+	private int happiness;
+	
+	//Assign numerical values to the different states
+	int happy = 5;
+	int content = 4;
+	int neutral = 3;
+	int sad = 2;
+	int sick = 1;
+	
+	
+	public Puppy() {
+		happiness = happy;
+	}
+	
+	
+}
