402838da07f052d6de1a519b7f0a8f8c0d4bfc80
quinnb27 <quinnb27@gmail.com>
2014-06-09 09:51:11 -0700

    Update ListPage.java

--- todolist/src/com/example/todolist/ListPage.java
+++ todolist/src/com/example/todolist/ListPage.java
@@ -1,5 +1,29 @@
 package com.example.todolist;
 
-public class ListPage {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.example.todolist.ListPage;
+
+public class ListPage extends Fragment {
+	
+	public static ListPage newInstance() {
+		ListPage fragment = new ListPage();
+		return fragment;
+	}
+
+	public ListPage() {
+		
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		
+		View rootView = inflater.inflate(R.layout.list_page, container, false);
+		
+		return rootView;
+	}
 }
