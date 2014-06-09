addd6b042b6cda73ca6cd556cf5ae8fc5ec156f9
quinnb27 <quinnb27@gmail.com>
2014-06-09 09:50:31 -0700

    Update PuppyPage.java

--- todolist/src/com/example/todolist/PuppyPage.java
+++ todolist/src/com/example/todolist/PuppyPage.java
@@ -1,5 +1,31 @@
 package com.example.todolist;
 
-public class PuppyPage {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.example.todolist.PuppyPage;
+
+public class PuppyPage extends Fragment {
+
+	/**
+	 * Returns a new instance of this fragment.
+	 */
+	public static PuppyPage newInstance() {
+		PuppyPage fragment = new PuppyPage();
+		return fragment;
+	}
+
+	public PuppyPage() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		
+		View rootView = inflater.inflate(R.layout.puppy_page, container, false);
+		
+		return rootView;
+	}
 }
